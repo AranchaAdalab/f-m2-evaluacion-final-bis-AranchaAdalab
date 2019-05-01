c773897a18238b75575b2256857e455707e2e9a8
AranchaAdalab <aranzazunarvaez@gmail.com>
2019-05-01 14:41:05 +0200

    clik changes photos

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,7 +4,7 @@ const start = document.querySelector('.start');
 const input = document.querySelectorAll('.input');
 let number = 4;
 const list = document.querySelector('.cards_list');
-const allPokemon = document.querySelectorAll('li');
+const photoAdalab = 'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';
 
 function numberCards(event) {
   const gilty = event.currentTarget;
@@ -19,14 +19,21 @@ function search () {
     .then (data => {
       for (const result of data) {
         const pokemon = result.image;
-        list.innerHTML += `<li class="item_pokemon"><img class="pokemon_photo" src="${pokemon}"></li>`;
+        list.innerHTML += `<li class="item_pokemon"><img class="pokemon_photo" src="${pokemon}"><img class="adalab_photo hidden" src="${photoAdalab}"></li>`;
+      }
+      const allPokemon = document.querySelectorAll('li');
+      for (const li of allPokemon) {
+        li.addEventListener('click', frontBack);
       }
     });
 }
 
 function frontBack(event) {
-  const gilty = event.currentTarget;
-  gilty.classList.toggle('back_card');
+  const li = event.currentTarget;
+  const photo = li.querySelector('.pokemon_photo');
+  const photoGreen = li.querySelector('.adalab_photo');
+  photo.classList.toggle('hidden');
+  photoGreen.classList.toggle('hidden');
 }
 
 for (const inputItem of input) {
@@ -34,7 +41,3 @@ for (const inputItem of input) {
 }
 
 start.addEventListener('click', search);
-
-for (const li of allPokemon) {
-  li.addEventListener('click', frontBack);
-}
